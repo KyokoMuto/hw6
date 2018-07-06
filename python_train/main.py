98a8f06bf6c7c3f76227c06a5c75d211be73c1c5
KyokoMuto <kymuto60@yahoo.co.jp>
2018-07-07 00:16:52 +0900

    next commit

--- python_train/main.py
+++ python_train/main.py
@@ -7,70 +7,66 @@ import cgi
 import json
 import codecs
 
-f=codecs.open("train.json","r","utf-8")
+f=codecs.open("train.json",'r',"utf-8")
 train=json.load(f)
 train_load={}
-for i,v in f.items():
-    if i=="stations":
-        for j in v:
-            train_load.setdefault(j,[]).append(before)
-            before=j
+
+for p in train:
+    for i,v in p.items():
+        if i=='Stations':  
+            before='none'
+            for j in v:
+                if before!='none':
+                    train_load.setdefault(j,[0]).append(before)
+                    train_load.setdefault(before,[0]).append(j)
+                before=j
 f.close
 
-def broken_train():
-    g=codecs.open("train_break.json","r","utf-8")
-    f_break=load(g)
-    for i in f:
-        if i=="stations":
-            for j in v:
-                train_load.setdefault(j,[]).append(before)
-                before=j
-    f.close
-
-
-
-
-
-JINJA_ENVIROMENT=jinja2.Enviroment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(_file_)),
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class BaseHandler(webapp2.RequestHandler)):
-    def render(self,html,values={}):
-        template=JINJA_ENVIROMENT.get_template(html)
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, html, values={}):
+        template = JINJA_ENVIRONMENT.get_template(html)
         self.response.write(template.render(values))
 
 class MainHandler(BaseHandler):
     def get(self):
-        self.render("main1.html")
+        self.render('main2.html')
+
 
 class SubHandler(BaseHandler):
     def get(self):
-        self.render("main2.html")
-        word1=List(cgi.escape(self.request.get("firstword")))
-        word2=List(cgi.escape(self.request.get("secondword")))
-        next_word=[word1]
-        way=[word1]
+        self.render("main3.html")
+        word1=self.request.get("firstword")
+        word2=self.request.get("secondword")
+        next_word=[word1] 
+        way=[word2]
         parent={}
         while(True):
             next=next_word[0]
             if next==word2:
                 break
             else:
-                for j in range(len(train_load[next])):
-                    next_word.append(train_load[next][j])
-                    parent[train_load[next][j]]=next
-        word=word2
-        while(true):
+                for j in range(1,len(train_load[next])):
+                    if train_load[train_load[next][j]][0]==0:
+                        next_word.append(train_load[next][j])
+                        train_load[train_load[next][j]][0]=1
+                        parent[train_load[next][j]]=next
+            del next_word[0]
+        word=word2    
+        while(True):
             way.append(parent[word])
             word=parent[word]
             if word==word1:
                 break
         for k in range(len(way)):
-            self.response.out.write(way[len(way)-k])
+            self.response.out.write(way[len(way)-1-k])
+            #print(way[len(way)-1-k])
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/',SubHandler),
+    ('/seni',SubHandler),
 ], debug=True)

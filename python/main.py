56fa2cf876c6f1cbaf301248cb37806bf22c6b07
KyokoMuto <kymuto60@yahoo.co.jp>
2018-07-06 16:26:20 +0900

    first commit

--- python/main.py
+++ python/main.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
+import jinja2
 import webapp2
+import cgi
 
-class MainPage(webapp2.RequestHandler):
+JINJA_ENVIROMENT=jinja2.Enviroment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(_file_)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class BaseHandler(webapp2.RequestHandler)):
+    def render(self,html,values={}):
+        template=JINJA_ENVIROMENT.get_template(html)
+        self.response.write(template.render(values))
+
+class MainHandler(BaseHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
-        self.response.write(u'こんにちは！')
+        self.render("main1.html")
+
+class SubHandler(BaseHandler):
+    def get(self):
+        self.render("main2.html")
+        word1=List(cgi.escape(self.request.get("word1")))
+        word2=List(cgi.escape(self.request.get("word2")))
+        n=len(word1)
+        m=len(word2)
+        mixword=""
+        for i in range(n):
+            mixword+=word1[i:i+1]+word2[i(m/n):i+1(m/n)]
+        self.response.out.write(mixword)
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', MainHandler),
+    ('/',SubHandler),
 ], debug=True)

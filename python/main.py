53862df4db303aa0d975c8df6ed2e83d636bcc2b
KyokoMuto <kymuto60@yahoo.co.jp>
2018-07-06 21:35:52 +0900

    python

--- python/main.py
+++ python/main.py
@@ -5,33 +5,40 @@ import jinja2
 import webapp2
 import cgi
 
-JINJA_ENVIROMENT=jinja2.Enviroment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(_file_)),
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class BaseHandler(webapp2.RequestHandler)):
-    def render(self,html,values={}):
-        template=JINJA_ENVIROMENT.get_template(html)
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, html, values={}):
+        template = JINJA_ENVIRONMENT.get_template(html)
         self.response.write(template.render(values))
 
 class MainHandler(BaseHandler):
     def get(self):
-        self.render("main1.html")
+        self.render('main2.html')
+ 
+#class MainHandler(webapp2.RequestHandler):
+ #   def get(self):
+  #      self.response.headers['Content-Type']='text/html'
+   #     self.render("main2.html")
+        #self.response.write("")
+        
 
 class SubHandler(BaseHandler):
     def get(self):
-        self.render("main2.html")
-        word1=List(cgi.escape(self.request.get("word1")))
-        word2=List(cgi.escape(self.request.get("word2")))
+        self.render("main3.html")
+        word1=self.request.get("firstword")
+        word2=self.request.get("secondword")
         n=len(word1)
         m=len(word2)
         mixword=""
         for i in range(n):
-            mixword+=word1[i:i+1]+word2[i(m/n):i+1(m/n)]
+            mixword+=word1[i:i+1]+word2[i*m/n:(i+1)*m/n]
         self.response.out.write(mixword)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/',SubHandler),
+    ('/seni',SubHandler),
 ], debug=True)
